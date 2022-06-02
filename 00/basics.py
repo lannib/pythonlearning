672021f56e559c3bbd462951d2dac01b1f6ce056
Ben Lanning <blanning2012@gmail.com>
2022-06-01 23:42:45 -0400

    code: updated the basics and the classics

---
+++ 00/basics.py
@@ -0,0 +1,38 @@
+# Comments
+# This is a comment. You can write whatever you want here
+# and it won't be executed.
+# These can be used for in-line documentation of your code
+
+# Printing
+# You can print strings
+print("hi") 
+print()
+# You can print integers
+print(5)
+print(235.45)
+# You can print other things
+
+5+10-2 #This won't display since we are not printing it
+
+# Math and types
+1 # integer. it is a whole number that can be negative
+-10 # still an integer
+
+5.1 # floats, floating point number (decimals)
+
+"Hi there 12345" # this is a string,m it can be any set of characters including whitespace
+print("\"") # You can escape special characters with a backslash
+
+True # this is a boolean, they are either true or false
+
+# Integer math, including string usage (characters for display purposes, not executed functions)
+print("5+5=", 5+5) # outputs 5+5= 10
+print("5-5+", 5-5)
+print("5/5=", 5/5) #division, yields float
+print("5/6=", 5/6) #division, yields float
+print("5//6=", 5//6) #integer division that truncates the result. returns an integer
+# modulus division (this yields the remainder of the division)
+print("5%3=",5%3) #should return 2
+print("3%5=",3%5) #should return 3 (essentially 0 remainder 3/5 so 3 is returned)
+print("5*5=",5*5)
+print("2**4=",2**4) #2 raised to the power of 4 (exponent)
